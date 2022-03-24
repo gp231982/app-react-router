1c956cdf0e7709840cb3cc1120a4bfb4b2bc4199
Grzegorz Piejko <gp23.1982@gmail.com>
2022-03-24 14:47:02 +0100

    Updates

---
+++ precache-manifest.069291720ea35ae7243befcabc5255ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "460e10d3ffacd302f7bc",
+    "url": "/app-react-router/static/css/main.3770850f.chunk.css"
+  },
+  {
+    "revision": "460e10d3ffacd302f7bc",
+    "url": "/app-react-router/static/js/main.841c39ed.chunk.js"
+  },
+  {
+    "revision": "d466ce07c2972432d57f",
+    "url": "/app-react-router/static/js/runtime~main.6a4469d8.js"
+  },
+  {
+    "revision": "09a14c385fde463d7484",
+    "url": "/app-react-router/static/js/2.0ba2db0f.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-react-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "a7f8b9ef68be244c7f9937ba8cc36187",
+    "url": "/app-react-router/index.html"
+  }
+];
